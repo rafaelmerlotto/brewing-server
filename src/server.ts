1df30f40e1c19b775fcd919a5215c9f2315ea68d
root <root@Rafael-Merlotto>
2023-10-03 10:00:56 +0200

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import express from 'express'
 import { app } from './app';
 import { auth } from './auth';
+import cors from 'cors'
 
 const server = express();
 server.use(express.json())
 server.use( app)
 server.use( auth)
+server.use(cors)
 
-const PORT = process.env.TZ || 3000;
+const PORT = process.env.TZ || 4000;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
 })
+
